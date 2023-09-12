83dd939600c86dd131ec7f881bf3e351ca1d45c7
Tanvirmehedi <tjmehedi201@gmail.com>
2023-09-12 07:16:26 +0600

    categories crud improve

--- server/services/categoryServices/getCategory.js
+++ server/services/categoryServices/getCategory.js
@@ -2,7 +2,21 @@
 
 import prisma from "../../config/prismaClient.js";
 
-
-export const getCategoris = async(req,res,next)=>{
-
-}
+export const getCategoris = async (req, res, next) => {
+  try {
+    const findCategories = await prisma.category.findMany({});
+    if (findCategories <= 0) {
+      const error = new Error("No Category Found");
+      error.status = 404;
+      next(error);
+      return;
+    }
+    res.json({
+      findCategories,
+    });
+  } catch (err) {
+    const error = new Error(err);
+    error.status = 500;
+    next(error);
+  }
+};
--- server/services/categoryServices/postCategory.js
+++ server/services/categoryServices/postCategory.js
@@ -2,7 +2,35 @@
 
 import prisma from "../../config/prismaClient.js";
 
+export const createCategory = async (req, res, next) => {
+  try {
+    const { name } = req.body;
 
-export const createCategory = async(req,res,next)=>{
+    const findCategoris = await prisma.category.findFirst({
+      where: {
+        name: name,
+      },
+    });
 
-}
+    if (!findCategoris) {
+      const newCategory = await prisma.category.create({
+        data: {
+          name: name,
+        },
+      });
+      res.json({
+        message: "Category Is SucessFully Created",
+        newCategory,
+      });
+      return;
+    }
+    res.json({
+      message: "Category Already Exsist",
+      category: findCategoris,
+    });
+  } catch (err) {
+    const error = new Error(err);
+    error.status = 500;
+    next(error);
+  }
+};
